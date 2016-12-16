0d4df390311a6e0969bb779dd375c1fa565c8269
shreeharshini <mamidalaharshini@gmail.com>
2016-12-16 13:26:58 +0530

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
  
+  get 'counter_and_custom_reports/index'
+
+  get 'counter_and_custom_reports/csv'
+
+  get 'counter_and_custom_reports/xlsx'
+
+  get 'counter_and_custom_reports/zip'
+
+
   get 'export_files/csv'
 
   get 'export_files/xlsx'
@@ -19,16 +28,18 @@ Rails.application.routes.draw do
   get "loginpage/accessdetails"
   get "loginpage/selectedplatforms"
 	get "home/index"
-
+  get "integration_partners/manage_sushi_account"
   get "import_data/xlsx"
+  post "search_by_issn/search"
   
   resources :loginpage, :accounts, :reports, :platforms, 
             :source_reports_mappings, :year_trends, :year_usages,
             :year_top_journals, :account_infos, :export_files, 
             :platform_reports, :requestfordemos, :contact_us, 
-            :library_account_details, :import_data
+            :library_account_details, :import_data, :search_by_issn , 
+            :integration_partners, :additional_users
 
-
+  devise_for :additional_users
   devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214091639) do
+ActiveRecord::Schema.define(version: 20161215095014) do
 
   create_table "account_infos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "account_code"
@@ -70,6 +70,33 @@ ActiveRecord::Schema.define(version: 20161214091639) do
     t.string   "activation_status"
     t.string   "account_code"
     t.integer  "user_id"
+  end
+
+  create_table "additional_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "role"
+  end
+
+  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "br1s", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
